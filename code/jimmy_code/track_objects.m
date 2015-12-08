778b387793862a92d4e03f0fa5043e6df3182e61
Jimmy Dieng <jimmy_dieng@hotmail.com>
2015-12-07 20:35:37 -0500

    Update track_objects.m

--- code/jimmy_code/track_objects.m
+++ code/jimmy_code/track_objects.m
@@ -1,3 +1,4 @@
+%% Modify these parameters to create the path
 FRAME_DIR = '../clip_2/';
 DET_DIR = './clip_2_faces/gender/';
 FINAL_OUTPUT = './clip_2_final_frames/%03d.jpg';
@@ -22,42 +23,39 @@ for i = start_frame:last_frame
 end
 end_index = last_frame - start_frame + 1;
 my_path = get_paths(all_features, all_images, 1, end_index);
-disp(size(my_path));
-
-
 
 num_paths = size(my_path, 3);
 gender_paths = [];
+
+% We don't want to try messing with paths that don't exist
 if size(my_path, 1) > 0
     for k = 1: num_paths
         cur_path = my_path(:, :, k);
+        % Paths must have the gender column
         if size(cur_path, 2) > 6
             amt_gender = sum(cur_path(cur_path(:, 1) > 0, 7));
             num_frames = size(cur_path(cur_path(:, 1) > 0, 7), 1);
             possible_gender = round(amt_gender / num_frames);
+            
+            % Here we are making the gender for each path consistent
+            % We do this by making the gender for the path whatever the majority is
             cur_path(cur_path(:, 1) > 0, 7) = possible_gender;
         end
         gender_paths = cat(3, gender_paths, cur_path);
     end
 end
 
-start_frame_str = int2str(start_frame);
-last_frame_str = int2str(last_frame);
-current_frame = strcat('./clip_2_paths/', start_frame_str, '-', last_frame_str, '_clip_2');
-
-save(current_frame, 'gender_paths');
-
-% for i=1:end_index
-%     features_used = [];
-%     if size(my_path, 1) > 0
-%         for k = 1:num_paths
-%             cur_path = gender_paths(i, :, k);
-%             features_used = [features_used; cur_path];
-%         end
-%     end
-%     f = figure; showboxes(all_images{i}, features_used);
-%     f = figure; showboxes(all_images{i}, all_features{i});
-%     saveas(f, sprintf(FINAL_OUTPUT, i + start_frame - 1));
-%     close all;
-% end
-% close all;
+% Construct the paths 
+for i=1:end_index
+    features_used = [];
+    if size(my_path, 1) > 0
+        for k = 1:num_paths
+            cur_path = gender_paths(i, :, k);
+            features_used = [features_used; cur_path];
+        end
+    end
+    f = figure; showboxes(all_images{i}, features_used);
+    saveas(f, sprintf(FINAL_OUTPUT, i + start_frame - 1));
+    close all;
+end
+close all;
